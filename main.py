a5f9636b12ed50590b50d0c505c6fce0e990c285
Ggosh1 <radiging@gmail.com>
2022-12-17 17:19:10 +0300

    ui file was converted to python class and color of circles is random now.

--- main.py
+++ main.py
@@ -3,12 +3,40 @@ import random
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class MyWidget(QMainWindow):
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(300, 390, 151, 91))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "круг"))
+
+
+class MyWidget(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('01.ui', self)
+        self.setupUi(self)
         self.pushButton.clicked.connect(self.run)
         self.paint = False
 
@@ -25,9 +53,9 @@ class MyWidget(QMainWindow):
             self.paint = False
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        r = random.randint(5, 500)
-        qp.drawEllipse(random.randint(0, self.width()), random.randint(0, self.height()), r, r)
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
+        r = random.randint(5, 200)
+        qp.drawEllipse(random.randint(r, self.width() - r), random.randint(r, self.height() - r), r, r)
 
 
 if __name__ == '__main__':
